c4550a53f908aafe19b15448987e93aff2674ef8
Yamileth Cabrera <cabrerayamileth@gmail.com>
2023-08-21 19:02:39 -0300

    Variables en JS

---
+++ Clase-1/index.js
@@ -0,0 +1,24 @@
+/*VARIABLES: Espacio en memoria para guardar informacion*/
+//var nombre = 'Osvaldito' // Esto es una mala practica no se deben declarar variables con var
+let nombre = 'Lionel'; // Si no le agrego el valor a la variable se va a definir como undefined
+let apellido = 'Messi';
+
+// Ctrl + alt + L  para hacer el Turbo Controle log
+console.log("🚀 ~ file: index.js:5 ~ apellido:", apellido);
+
+let edad = 37;
+//Asi puedo reasignar el valor a la variable edad
+edad = 38; 
+console.log("🚀 ~ file: index.js:9 ~ edad:", edad);
+
+/*Constantes*/
+const pi = 3.14;
+console.log("🚀 ~ file: index.js:16 ~ pi:", pi)
+
+
+
+
+/*Para comentar varias líneas en Visual Studio Code, basta con seleccionar 
+las líneas que se desean comentar y presionar las teclas Ctrl + K + C en Windows 
+o Linux. Por otro lado, para descomentarlas, se debe presionar Ctrl + K + U 
+en Windows o Linux*/ 
